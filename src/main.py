09c58bfee629b8ee7fcc688fbfa426216338740d
Frederik Mars <10710973+fr3dch3n@users.noreply.github.com>
2025-12-02 18:20:49 +0100

    chore: remove obsolete main

--- src/main.py
+++
@@ -1,6 +0,0 @@
-def hello() -> str:
-    return "Hello, Advent of Code!"
-
-
-if __name__ == "__main__":
-    print(hello())
--- tests/test_main.py
+++
@@ -1,5 +0,0 @@
-from src.main import hello
-
-
-def test_hello():
-    assert hello() == "Hello, Advent of Code!"
